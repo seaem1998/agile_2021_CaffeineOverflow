bfac219c4f2aa00aaf33605b3fdd35c92f3284e0
seaem1998 <chantellemarek@yahoo.com>
2021-02-18 12:49:17 -0600

    Activity 2 - Submits information added to it. Will need to edit the code to make it display properly

--- app/src/main/java/edu/cvtc/cmarek5/SecondActivity.java
+++ app/src/main/java/edu/cvtc/cmarek5/SecondActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -23,12 +25,36 @@ public class SecondActivity extends AppCompatActivity {
         super.onStart();
         Log.d(TAG, "Component Has Started");
         Button submitButton = findViewById(R.id.button_contact_submit);
+        Button resultButton = findViewById(R.id.button_calcPayment);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
                 Log.d(TAG, "Button Clicked");
+                //Takes User back to the Home Page
                 startActivity(new Intent(SecondActivity.this, MainActivity.class));
+
+                // Get the Edit Texts --Chantelle
+                EditText NameText = ((View)view.getParent()).findViewById(R.id.editText_Name);
+                EditText TotalText = ((View)view.getParent()).findViewById(R.id.editText_Total);
+
+                // Toast to Display the User Inputs
+                Toast.makeText(SecondActivity.this, "Your name is " + NameText + ". Your" +
+                        " total is " + TotalText, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        resultButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                Log.d(TAG, "Button Clicked");
+                // Get the Edit Texts --Chantelle
+                EditText NameText = ((View)view.getParent()).findViewById(R.id.editText_Name);
+                EditText TotalText = ((View)view.getParent()).findViewById(R.id.editText_Total);
+
+                // Toast to Display the User Inputs
+                Toast.makeText(SecondActivity.this, "Your name is " + NameText + ". Your" +
+                        " total is " + TotalText, Toast.LENGTH_LONG).show();
             }
         });
     }
