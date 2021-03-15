b9eddcbd6464b4a54adab7046b0f92c2d05e41fc
seaem1998 <chantellemarek@yahoo.com>
2021-03-15 15:11:22 -0500

    Completed the Sales Tax and Tip calculator. Adjusted the Contact page. It shifted when ran in the emulator. It is adjusted as best as it can.

--- app/src/main/java/edu/cvtc/cmarek5/SalesTaxCalcActivity.java
+++ app/src/main/java/edu/cvtc/cmarek5/SalesTaxCalcActivity.java
@@ -3,8 +3,16 @@ package edu.cvtc.cmarek5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SalesTaxCalcActivity extends AppCompatActivity {
+
+    // Variables
+    private EditText cost;
+    private EditText tax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,37 @@ public class SalesTaxCalcActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sales_tax_calc);
     }
 
-    // I need to multiply the decimal by one hundred to get the right calculations --Chantelle
+    protected void onStart() {
+        super.onStart();
+
+        // The Sales Tax Button
+        Button taxButton = findViewById(R.id.button_sales_tax_submit);
+
+        taxButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cost = (EditText) findViewById(R.id.cost_text);
+                tax = (EditText) findViewById(R.id.tax_text);
+
+                // Convert to string
+                String sCost = cost.getText().toString();
+                String sTax = tax.getText().toString();
+
+                // Convert to Double
+                Double dCost = Double.parseDouble(sCost);
+                Double dTax = Double.parseDouble(sTax);
+
+                    // Calculate the Cost of the Bill
+                    double taxPercent = (dTax / 100);
+                    double calcPercent = dCost * taxPercent;
+                    double payment = dCost + calcPercent;
+
+                    // Convert Payment to a String
+                    String billDue = (String.valueOf(payment));
+
+                    // Send to Toast to show the user their amount
+                    Toast.makeText(SalesTaxCalcActivity.this, "Your Total is " + billDue, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
--- app/src/main/java/edu/cvtc/cmarek5/TipCalcActivity.java
+++ app/src/main/java/edu/cvtc/cmarek5/TipCalcActivity.java
@@ -3,14 +3,75 @@ package edu.cvtc.cmarek5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TipCalcActivity extends AppCompatActivity {
+
+    // Create Variables --Chantelle
+    private EditText amount; // The Bill, before the tip
+    private TextView seekBarPercent; // The percentage choosen for the tip
+    private Button calculate; // Function to calculate the tip
+    private SeekBar seekBar; // The Seek Bar itself
+    private int seekPercent;
+    private double bill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calc);
-    }
 
-    // I need to multiply the decimal by one hundred to get the right calculations --Chantelle
+        // Connect the variables to where they draw user data from -- Chantelle
+        amount = (EditText) findViewById(R.id.tip_calc_total_amount_text);
+        seekBar = (SeekBar) findViewById(R.id.seekBar_percent);
+        seekBarPercent = (TextView) findViewById(R.id.percentage_label);
+        calculate = (Button) findViewById(R.id.tip_button);
+
+        // This will keep track of what the Seek Bar does -- Chantelle
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Shows the percentage that Seek Bar is on
+                seekBarPercent.setText(String.valueOf(seekBar.getProgress()) + "%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Gets the progress of the seek bar
+                seekPercent = seekBar.getProgress();
+            }
+        });
+
+        // This will calculate the Total Cost of the Bill, this will include the tip
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If the user provided information, math will be performed
+                if (!amount.getText().toString().equals("")) {
+                    // Variables to perform calculations
+                    double result = 0;
+                    double total = 0;
+
+                    // Calculate the Total
+                    bill = Double.parseDouble(amount.getText().toString());
+                    result = bill * seekPercent / 100;
+                    total = bill + result;
+
+                    Toast.makeText(TipCalcActivity.this, "Total is " + total, Toast.LENGTH_LONG).show();
+                } else {
+                    // If user provided no information, we do nothing and let them know to add information
+                    Toast.makeText(TipCalcActivity.this, "Please Enter the Amount" ,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
