4ce1141b13403a5ad63ab65ded868f57f522ff31
Devin <devinprice@live.com>
2021-03-19 13:12:07 -0500

    Validation and user input now connected to the chart.

--- app/src/main/java/edu/cvtc/cmarek5/BudgetChartActivity.java
+++ app/src/main/java/edu/cvtc/cmarek5/BudgetChartActivity.java
@@ -10,8 +10,10 @@ import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BudgetChartActivity extends AppCompatActivity {
@@ -26,74 +28,46 @@ public class BudgetChartActivity extends AppCompatActivity {
 
         Intent categoryIntent = getIntent();
         String [] userCategories = categoryIntent.getStringArrayExtra("categories_key");
+        String [] userCapacities = categoryIntent.getStringArrayExtra("capacities_key");
+
+        // tests for user input
+//        String categoryString = Arrays.toString(userCategories);
+//        String capacitiesString = Arrays.toString(userCapacities);
+//        Toast.makeText(getApplicationContext(), capacitiesString, Toast.LENGTH_LONG).show();
 
         pieChart = (PieChart) findViewById(R.id.idPieChart);
         pieChart.setRotationEnabled(true);
         pieChart.setHoleRadius(25f);
         pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("Example Chart");
+        pieChart.setCenterText("User Budget");
         pieChart.setCenterTextSize(16);
         pieChart.setUsePercentValues(true);
         //pieChart.setDrawEntryLabels(true);
 
-        addDataSet();
+        addDataSet(userCategories, userCapacities);
 
     }
 
-    private void addDataSet() {
+    private void addDataSet(String [] categories, String [] capacities) {
+
 
         List<PieEntry> entries = new ArrayList<>();
 
-        
-
-        entries.add(new PieEntry(18.5f, "Groceries"));
-        entries.add(new PieEntry(20.0f, "Entertainment"));
-        entries.add(new PieEntry(24.0f, "Insurance"));
-        entries.add(new PieEntry(18.4f, "Gas"));
-        entries.add(new PieEntry(12.4f, "Utilities"));
-        entries.add(new PieEntry(6.7f, "Clothing"));
+        entries.add(new PieEntry(Integer.parseInt(capacities[0]), categories[0]));
+        entries.add(new PieEntry(Integer.parseInt(capacities[1]), categories[1]));
+        entries.add(new PieEntry(Integer.parseInt(capacities[2]), categories[2]));
+        entries.add(new PieEntry(Integer.parseInt(capacities[3]), categories[3]));
         PieDataSet set = new PieDataSet(entries, "User Budget");
         PieData data = new PieData(set);
+        data.setValueFormatter(new PercentFormatter(pieChart));
         data.setValueTextColor(Color.BLACK);
-        data.setValueTextSize(14f);
+        data.setValueTextSize(18f);
         set.setValueTextColor(Color.BLACK);
         set.setColors(Color.RED, Color.GREEN, Color.MAGENTA, Color.BLUE, Color.CYAN, Color.LTGRAY);
 
         pieChart.setData(data);
         pieChart.invalidate(); // refresh
 
-//        ArrayList<PieEntry> yEntries = new ArrayList<>();
-//        ArrayList<String> xEntries = new ArrayList<>();
-//
-//        for (int i = 0; i < yData.length; i++){
-//            yEntries.add(new PieEntry(yData[i], i));
-//        }
-//
-//        for (int i = 1; i < xData.length; i++){
-//            xEntries.add(xData[i]);
-//        }
-//
-//        PieDataSet pieDataSet = new PieDataSet(yEntries, "Example");
-//        pieDataSet.setSliceSpace(2);
-//        pieDataSet.setValueTextSize(12);
-//
-//        //adding colors
-//        ArrayList<Integer> colors = new ArrayList<>();
-//        colors.add(Color.GRAY);
-//        colors.add(Color.BLUE);
-//        colors.add(Color.RED);
-//        colors.add(Color.YELLOW);
-//        colors.add(Color.GREEN);
-//        colors.add(Color.CYAN);
-//        colors.add(Color.MAGENTA);
-//
-//        //adding legend to chart
-//        Legend legend = pieChart.getLegend();
-//        legend.setForm(Legend.LegendForm.CIRCLE);
-//
-//        //PieData pieData = new PieDataSet(pieDataSet);
-//        //pieChart.setData(pieData);
-//        pieChart.invalidate();
 
     }
 }
--- app/src/main/java/edu/cvtc/cmarek5/BudgetCreatorActivity.java
+++ app/src/main/java/edu/cvtc/cmarek5/BudgetCreatorActivity.java
@@ -10,13 +10,29 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class BudgetCreatorActivity extends AppCompatActivity {
+
+    EditText category1, category2, category3, category4, categoryCap1, categoryCap2, categoryCap3, categoryCap4;
+    boolean areCategoriesValid = false, areCategoriesCapValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
+        //adding category variables for validation
+        category1 = (EditText) findViewById(R.id.userCategory1);
+        category2 = (EditText) findViewById(R.id.userCategory2);
+        category3 = (EditText) findViewById(R.id.userCategory3);
+        category4 = (EditText) findViewById(R.id.userCategory4);
+        categoryCap1 = (EditText) findViewById(R.id.categoryCap1);
+        categoryCap2 = (EditText) findViewById(R.id.categoryCap2);
+        categoryCap3 = (EditText) findViewById(R.id.categoryCap3);
+        categoryCap4 = (EditText) findViewById(R.id.categoryCap4);
+
     }
 
 
@@ -28,21 +44,77 @@ public class BudgetCreatorActivity extends AppCompatActivity {
         createBudgetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
-                String listOfCategories[] = {String.valueOf(R.id.userCategory1), String.valueOf(R.id.userCategory2), String.valueOf(R.id.userCategory3), String.valueOf(R.id.userCategory4)};
-                Integer listOfCapacities[] = {R.id.categoryCap1, R.id.categoryCap2, R.id.categoryCap3, R.id.categoryCap4};
+                SetValidation();
+
+                String listOfCategories[] = {category1.getText().toString(), category2.getText().toString(), category3.getText().toString(), category4.getText().toString()};
+                String listOfCapacities[] = {categoryCap1.getText().toString(), categoryCap2.getText().toString(), categoryCap3.getText().toString(), categoryCap4.getText().toString()};
 
                 Intent categoryIntent = new Intent(getApplicationContext(), BudgetChartActivity.class);
+                Intent failedIntent = new Intent(getApplicationContext(), BudgetCreatorActivity.class);
                 categoryIntent.putExtra("categories_key", listOfCategories);
+                categoryIntent.putExtra("capacities_key", listOfCapacities);
 
-                Intent capacityIntent = new Intent(getApplicationContext(), BudgetChartActivity.class);
-                capacityIntent.putExtra("categories_key", listOfCapacities);
+//                Intent capacityIntent = new Intent(getApplicationContext(), FifthActivity.class);
+//                capacityIntent.putExtra("categories_key", listOfCapacities);
 
-
-                // This act like Navigation, Takes you to the second activity
-                startActivity(new Intent(BudgetCreatorActivity.this, BudgetChartActivity.class));
-
+                if (areCategoriesValid != false && areCategoriesCapValid != false) {
+                    startActivity(categoryIntent);
+                }
             }
         });
+
+    }
+
+    private void SetValidation() {
+        // Check and validate
+
+        if (category1.getText().toString().isEmpty()) {
+            category1.setError("Category cannot be empty");
+            category1.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please add a category", Toast.LENGTH_LONG).show();
+            areCategoriesValid = false;
+        } else if (category2.getText().toString().isEmpty()) {
+            category2.setError("Category cannot be empty");
+            category2.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please add a category", Toast.LENGTH_LONG).show();
+            areCategoriesValid = false;
+        } else if (category3.getText().toString().isEmpty()) {
+            category3.setError("Category cannot be empty");
+            category3.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please add a category", Toast.LENGTH_LONG).show();
+            areCategoriesValid = false;
+        } else if (category4.getText().toString().isEmpty()) {
+            category4.setError("Category cannot be empty");
+            category4.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please add a category", Toast.LENGTH_LONG).show();
+            areCategoriesValid = false;
+        } else {
+            areCategoriesValid = true;
+        }
+
+        if (categoryCap1.getText().toString().isEmpty()) {
+            categoryCap1.setError("Please enter a budget for this category");
+            categoryCap1.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please enter a budget for this category", Toast.LENGTH_LONG).show();
+            areCategoriesCapValid = false;
+        } else if (categoryCap2.getText().toString().isEmpty()) {
+            categoryCap2.setError("Please enter a budget for this category");
+            categoryCap2.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please enter a budget for this category", Toast.LENGTH_LONG).show();
+            areCategoriesCapValid = false;
+        } else if (categoryCap3.getText().toString().isEmpty()) {
+            categoryCap3.setError("Please enter a budget for this category");
+            categoryCap3.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please enter a budget for this category", Toast.LENGTH_LONG).show();
+            areCategoriesCapValid = false;
+        } else if (categoryCap4.getText().toString().isEmpty()) {
+            categoryCap4.setError("Please enter a budget for this category");
+            categoryCap4.requestFocus();
+            Toast.makeText(getApplicationContext(), "Please enter a budget for this category", Toast.LENGTH_LONG).show();
+            areCategoriesCapValid = false;
+        } else {
+            areCategoriesCapValid = true;
+        }
 
     }
 
@@ -62,33 +134,33 @@ public class BudgetCreatorActivity extends AppCompatActivity {
         return true;
 
     }
-
-    // The intent that takes us to the specific activity (Main Menu) --Chantelle
+//
+//    // The intent that takes us to the specific activity (Main Menu) --Chantelle
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {
             case R.id.action_home:
-                Intent intentActivityMain = new Intent(BudgetCreatorActivity.this, MainActivity.class);
+                Intent intentActivityMain = new Intent(getApplicationContext(), MainActivity.class);
                 intentActivityMain.putExtra("From Fourth Activity", "To Main Activity");
                 startActivity(intentActivityMain);
                 return true;
             case R.id.action_activity_three:
-                Intent intentActivityThree = new Intent(BudgetCreatorActivity.this, DebtsActivity.class);
+                Intent intentActivityThree = new Intent(getApplicationContext(), DebtsActivity.class);
                 intentActivityThree.putExtra("From Fourth Activity", "To Third Activity");
                 startActivity(intentActivityThree);
                 return true;
             case R.id.action_activity_two:
-                Intent intentActivityTwo = new Intent(BudgetCreatorActivity.this, ContactActivity.class);
+                Intent intentActivityTwo = new Intent(getApplicationContext(), ContactActivity.class);
                 intentActivityTwo.putExtra("From Fourth Activity", "To Second Activity");
                 startActivity(intentActivityTwo);
                 return true;
             case R.id.action_salex_tax:
-                Intent intentSalesTax = new Intent(BudgetCreatorActivity.this, SalesTaxCalcActivity.class);
+                Intent intentSalesTax = new Intent(getApplicationContext(), SalesTaxCalcActivity.class);
                 intentSalesTax.putExtra("From Fourth Activity", "To Sales Tax Calculator");
                 startActivity(intentSalesTax);
                 return true;
             case R.id.action_tip_calc:
-                Intent intentTipCalc = new Intent(BudgetCreatorActivity.this, TipCalcActivity.class);
+                Intent intentTipCalc = new Intent(getApplicationContext(), TipCalcActivity.class);
                 intentTipCalc.putExtra("From Fourth Activity", "To Tip Calculator");
                 startActivity(intentTipCalc);
                 return true;
